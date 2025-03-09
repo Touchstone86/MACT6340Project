940feef3b38787fa2b767ce098390cbf0d229d71
Touchstone86 <snjones@smu.edu>
2025-03-09 15:45:14 -0500

    added additional app.js settings

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+//app.get("/", (req, res) => {
+//    res.send("Hello World!");
+//});
+
+
+
+
 
 app.listen(port, () => {
+    console.log(process.env.SENSITIVE_INFO);
     console.log(`Example app listening at ${port}`);
 });
