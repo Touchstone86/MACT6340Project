e0b8377e865847cddcc3166e597f5ffdf3d4dee8
Touchstone86 <snjones@smu.edu>
2025-03-12 12:39:04 -0500

    updated project.ejs with links to project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
@@ -13,11 +14,16 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/project", (req, res) => {
-    res.render("project.ejs");
-  });
 app.get("/projects", (req, res) => {
-    res.render("projects.ejs");
+    res.render("projects.ejs",{projectArray:data});
+  });
+
+app.get("/project/:id", (req, res) => {
+    let id = req.params.id;
+    if (id> data.length) {
+        throw new Error("No Project ID found");
+    }
+    res.render("project.ejs",{projectArray: data, which: id});
   });
 
 app.get("/contact", (req, res) => {
